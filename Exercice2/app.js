cd1bb32388c72b3b9e3fe1deed4920b0ce7d3ac8
Charles Marcoin <charles.marcoin@gmail.com>
2022-03-30 16:00:44 +0200

    first attempt

---
+++ Exercice2/app.js
@@ -0,0 +1,50 @@
+const axios = require("axios");
+
+const COMPANY_NAMES = ["Swile", "Backmarket", "Ledger"];
+
+let broadCastArray = [];
+
+const buildMessages =  async function() {
+  for (companyName of COMPANY_NAMES) {
+    let openJobs = [];
+
+    // TODO : use .env for the domain
+    const endPoint = `http://localhost:5000/api/jobs/getOpenJobsInfos/${companyName.toLowerCase()}`;
+    console.log(endPoint)
+
+    try {
+        const response = await axios.get(endPoint);
+    
+        openJobs = response.data.map((job) => {
+            //console.log(openJobs)
+        });
+    
+        nbrOpenJobThisWeek = response.data.length;
+
+        // build the slack message here and then put it in the message variable
+        const message = {
+            nbrOpenJobThisWeek,
+            companyName,
+          };
+  
+          broadCastArray.push(message);
+
+    
+      } catch (err) {
+        //console.log(err)
+      }
+
+      return broadCastArray
+      
+
+  };
+};
+
+
+const start = async function() {
+    const broadCastArray = await buildMessages()
+}
+
+start()
+
+
